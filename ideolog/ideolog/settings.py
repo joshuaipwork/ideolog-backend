c174f6a8e2819afb95fecfd5d4f2933f1da3c2ee
Norton Pengra <npengra317@gmail.com>
2020-10-17 14:27:43 -0700

    cheap attempt at security and then decimating that attempt

--- ideolog/ideolog/settings.py
+++ ideolog/ideolog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e7$nq=_@czv%q%=(&ysot-!kqqobhr(@^4+n-@)sq*z_sjz@hu'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'e7$nq=_@czv%q%=(&ysot-!kqqobhr(@^4+n-@)sq*z_sjz@hu'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
